7b183b466d09c34dbd15b3b77af6dfba4e19554e
Pedr00ga <plgonzales4760@gmail.com>
2024-08-23 14:58:38 -0300

    Add files via upload

---
+++ Media.py
@@ -0,0 +1,20 @@
+
+def medias(notaa, notab, notac):
+    media = (notaa + notab + notac)
+    return media / 3
+    
+
+print (medias(7, 9, 10))
+#notam = float(input("Digite a nota de matematica: "))
+#notap = float(input("Digite a nota de portugues: "))
+#notaa = float(input("Digite a nota de artes: "))
+
+#quantidade = 3
+
+# Calculo da media
+#media = (notas + notas + notas)
+#resultado = media / quantidade
+
+
+
+
